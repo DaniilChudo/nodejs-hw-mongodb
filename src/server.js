02e18c6b102c5210b07771eb91081275f202dc2e
DaniilChudo <daniilpro1994@gmail.com>
2024-11-25 15:25:02 +0100

    Step_4,5,6

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
+import { getAllContacts } from './services/contacts.js';
 
 const logger = pino();
 const app = express();
@@ -18,3 +19,33 @@ export function setupServer() {
     logger.info(`Server is running on port ${port}`);
   });
 }
+
+app.get('/contacts', async (req, res) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/contacts/:contactId', async (req, res) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
