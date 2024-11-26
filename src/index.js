598b76974d955c98d9110c62a98972e6c36ed57c
DaniilChudo <daniilpro1994@gmail.com>
2024-11-26 16:57:14 +0100

    final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
-
 initMongoConnection();
 setupServer();
+console.log('MONGODB_USER:', process.env.MONGODB_USER);
