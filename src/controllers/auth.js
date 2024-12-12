1ac414e18bcf8d1f59a4b52a06a1c8eff119e62d
DaniilChudo <daniilpro1994@gmail.com>
2024-12-12 12:05:51 +0100

    3commit

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,34 +5,31 @@ import createError from 'http-errors';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 export const register = ctrlWrapper(async (req, res) => {
-  const { email, password, username } = req.body;
+  const { email, password, username, name } = req.body;
 
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    throw createError(400, 'User with this email already exists');
+    throw createError(409, 'Email in use');
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = await User.create({
+    name,
     email,
     password: hashedPassword,
     username,
   });
 
-  const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
-  });
-
   res.status(201).json({
     status: 201,
-    message: 'User successfully registered',
+    message: 'Successfully registered a user!',
     data: {
       user: {
+        name: newUser.name,
         email: newUser.email,
         username: newUser.username,
       },
-      token,
     },
   });
 });
@@ -50,20 +47,27 @@ export const login = ctrlWrapper(async (req, res) => {
     throw createError(401, 'Invalid email or password');
   }
 
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
+  const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '15m',
   });
+  const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  await Session.findOneAndDelete({ userId: user._id });
+  const newSession = new Session({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: Date.now() + 15 * 60 * 1000,
+    refreshTokenValidUntil: Date.now() + 30 * 24 * 60 * 60 * 1000,
+  });
+  await newSession.save();
 
   res.status(200).json({
     status: 200,
-    message: 'User successfully logged in',
-    data: {
-      user: {
-        email: user.email,
-        username: user.username,
-      },
-      token,
-    },
+    message: 'Successfully logged in an user!',
+    data: { accessToken },
   });
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,16 +3,16 @@ import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
       unique: true,
     },
     password: {
-      type: String,
-      required: true,
-    },
-    username: {
       type: String,
       required: true,
     },
