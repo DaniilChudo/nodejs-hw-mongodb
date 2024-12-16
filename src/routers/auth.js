b07ff3e529c091f8e0123816c232cdddc17e359b
DaniilChudo <daniilpro1994@gmail.com>
2024-12-16 14:39:14 +0100

    19commit

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,5 @@
 import express from 'express';
-import {
-  register,
-  login,
-  refreshSession,
-  logout,
-} from '../controllers/auth.js';
+import { register, login, refresh, logout } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validate.js';
 import { registerSchema, loginSchema } from '../middlewares/validate.js';
 
@@ -12,7 +7,7 @@ const router = express.Router();
 
 router.post('/register', validateBody(registerSchema), register);
 router.post('/login', validateBody(loginSchema), login);
-router.post('/refresh', refreshSession);
+router.post('/refresh', refresh);
 router.post('/logout', logout);
 
 export default router;
